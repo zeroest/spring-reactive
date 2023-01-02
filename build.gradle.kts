fa6d1bfdc03c86ceea8667b160803d6c475c213c
zeroest <zeroest.me@gmail.com>
2023-01-02 22:27:52 +0900

    default gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,70 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    java
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.6.21"
+
+    kotlin("kapt") version "1.6.21"
 }
 
-group = "me.zeroest"
-version = "0.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-repositories {
-    mavenCentral()
+
+allprojects {
+    group = "me.zeroest"
+    version = "0.0.0"
+
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-spring")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin-kapt")
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.projectreactor:reactor-test")
+
+        // H2DB
+        runtimeOnly("com.h2database:h2")
+
+        // Kotlin 로깅
+        implementation("io.github.microutils:kotlin-logging:1.12.5")
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
 }
